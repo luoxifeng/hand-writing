15a52f7020dab02d3718d5c899fcf1b59f46b8ce
luoxifeng <942394682@qq.com>
2021-06-15 17:18:35 +0800

    Update temp.js

--- 数据结构/temp.js
+++ 数据结构/temp.js
@@ -13,16 +13,50 @@ class LinkedList {
   // 向链表尾部添加一个元素
   append(element) {
     const node = new Node(element)
-    if (head === null) {
-      head = node
+    if (this.head === null) {
+      this.head = node
     } else {
-      let current = head.next
+      let current = this.head
       while (current.next) {
         current = current.next
       }
       current.next = node
     }
-    length++
+    this.length++
+  }
+
+  // 删除指定位置元素
+  removeAt(position) {
+    if (position < 0 || position >= this.length || this.head === null) return null
+    let current = this.head
+    if (position === 0) {
+      this.head = current.next
+    } else {
+      let index = 0;
+      let prev = null
+      while (index++ < position) {
+        prev = current
+        current = current.next
+      }
+      prev.next = current.next
+    }
+    this.length--
+    return current.element
+  }
+
+  // 删除第一个
+  removeHead() {
+    return this.removeAt(0)
+  }
+
+  // 删除最后一个
+  removeTail() {
+    return this.removeAt(this.length - 1)
   }
 
 }
+
+var list = new LinkedList()
+
+list.append(0)
+list.append(1)
