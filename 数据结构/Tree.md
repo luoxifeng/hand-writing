ff45b22b23b08e0e540d54d47e099d9ad60d2b3f
luoxifeng <942394682@qq.com>
2021-06-21 17:06:29 +0800

    tree

---
+++ 数据结构/Tree.md
@@ -0,0 +1,16 @@
+# Tree
+
+## BinarySearchTree 二叉搜索树
+- 类型约束
+```ts
+interface ITreeNode {
+  key: any;
+  left: ITreeNode;
+  right: ITreeNode
+}
+
+```
+
+- 实现
+```ts
+```
--- 数据结构/temp.ts
+++ 数据结构/temp.ts
@@ -1,1 +1,23 @@
 
+interface ITreeNode {
+  key: any;
+  left: ITreeNode;
+  right: ITreeNode
+}
+
+class TreeNode implements ITreeNode {
+
+  key = null
+  
+  left: ITreeNode = null
+
+  right: ITreeNode = null
+
+  constructor(key) {
+    this.key = key
+  }
+
+}
+class BinarySearchTree {
+
+}
